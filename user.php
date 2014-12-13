1bf2de610c57e596fff751d1ce1486937d24b3cc
Joe Looney <hello@joelooney.org>
2014-12-13 18:51:33 -0500

    Create user.php

---
+++ user.php
@@ -0,0 +1,14 @@
+<?php
+
+//Enter your Coinbase API Key and Secret
+$_API_KEY = "";
+$_API_SECRET = "";
+
+//Enter your Bitreseruve USD Card address
+$br_usd_addr = "";
+
+//Enter your pin/password
+$pin_secret = "";
+
+
+?>

e73a7266633f6ff3c7fb9bc93b04ffa2851fd8e6
Joe Looney <hello@joelooney.org>
2014-12-14 23:32:42 -0500

    Update cbsync.php

--- cbsync.php
+++ cbsync.php
@@ -9,6 +9,12 @@ $usdamt = $_POST["usdamt"];
 $brusd = $_POST["brusd"];
 $pin = $_POST["pin"];
 
+$brcny = $_POST["brcny"];
+$breur = $_POST["breur"];
+$brgold = $_POST["brgold"];
+
+$assettype = $_POST["assettype"];
+
 if (isset($usdamt) && $pin == $pin_secret){
 
 	$buy = $coinbase->getBuyPrice();
@@ -23,6 +29,8 @@ if (isset($usdamt) && $pin == $pin_secret){
 	echo "Buying ".$buyamt." BTC from Coinbase with Bank account...";
 	echo "<br>Success... ";
 	echo $user->success ? 'true' : 'false';
+	//echo "<br>Status... ";
+	//echo $user->transfer->status;
 	echo "<br>Total... $";
 	echo $user->transfer->total->amount;
 	echo "<br>";
@@ -30,13 +38,34 @@ if (isset($usdamt) && $pin == $pin_secret){
 	echo "<br>";
 
 
-	// send BTC to Bitreserve USD card
+	// send BTC to Bitreserve card
 	// ---
 	
-	echo "Sending ".$buyamt." BTC to Bitreserve USD card...";
+	$received = (round(($buyamt * $brusd),2))-.01;
+	
+	if ($assettype == "USD"){
+		$br_addr = $br_usd_addr;
+	}
+	if ($assettype == "CNY"){
+		$assetamount = $received / $brcny;
+		$br_addr = $br_cny_addr;
+	} 
+	if ($assettype == "EUR"){
+		$assetamount = $received / $breur;
+		$br_addr = $br_eur_addr;
+	} 
+	if ($assettype == "Gold"){
+		$assetamount = $received / $brgold;
+		$br_addr = $br_gold_addr;
+	} 
+	
+	
+	echo "Sending ".$buyamt." BTC to Bitreserve ".$assettype." card...";
 	echo "<br>Success... ";
-	$response = $coinbase->sendMoney($br_usd_addr, $buyamt, "to bitreserve");
+	$response = $coinbase->sendMoney($br_addr, $buyamt, "to bitreserve");
 	echo $response->success ? 'true' : 'false';
+	//echo "<br>Status... ";
+	//echo $response->transaction->status;
 	echo "<br>";
 	echo "--";
 
@@ -45,15 +74,23 @@ if (isset($usdamt) && $pin == $pin_secret){
 	// ---
 
 	$spent = $user->transfer->total->amount;
-	$received = (round(($buyamt * $brusd),2))-.01;
+	
 
-  echo "<br>Sent from Bank Account... $".$spent;
-  echo "<br>Received at Bitreserve... $".$received;    
+    echo "<br>Sent from Bank Account... $".$spent."<br>";
+    
+	if ($assettype == "USD") {
+		echo "Received at Bitreserve... $".$received;
+	} else {
+		echo "Received at Bitreserve... ~".round($assetamount,2)." ".$assettype." ($".$received.")";	
+	}
+    
 	echo "<br>";
 	echo "--";
 	echo "<br>Total Fee... $".round(($spent-$received),2);
 
-} else {
+
+
+}else {
 	echo "An Error has Occurred";
 }
 ?>

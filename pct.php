0700ce7e1abe50f15aa9c455f75ed3b7d0c483aa
Joe Looney <hello@joelooney.org>
2014-12-14 23:30:24 -0500

    Update pct.php

--- pct.php
+++ pct.php
@@ -7,17 +7,33 @@ function GetJsonFeed($json_url)
 };
 
 $buy = GetJsonFeed("https://api.coinbase.com/v1/prices/buy");
-$cbusd = $buy['total']['amount'];
+
 
 $brticker = GetJsonFeed("https://api.bitreserve.org/v0/ticker");
 foreach ($brticker as $tick) {
 	if ($tick['currency'] == "USD" && $tick['pair'] == "BTCUSD"){
 		$brusd = round((($tick['ask'] + $tick['bid'])/2)-(0.0045*(($tick['ask'] + $tick['bid'])/2)),2);
 	}
+	if ($tick['currency'] == "USD" && $tick['pair'] == "EURUSD"){
+		$breur = round((($tick['ask'] + $tick['bid'])/2)-(0.0095*(($tick['ask'] + $tick['bid'])/2)),4);		
+	}
+	if ($tick['currency'] == "CNY" && $tick['pair'] == "USDCNY"){
+		$brcny = (($tick['ask'] + $tick['bid'])/2)-(0.02*(($tick['ask'] + $tick['bid'])/2));		
+	}
+	if ($tick['currency'] == "USD" && $tick['pair'] == "XAUUSD"){
+		$brgold = round((($tick['ask'] + $tick['bid'])/2)-(0.024*(($tick['ask'] + $tick['bid'])/2)),4);		
+	}
+	
+	
 }
-
-$pctcost['buy'] = $cbusd;
+$pctcost['buy'] = $buy['total']['amount'];
 $pctcost['sell'] = $brusd;
+
+$pctcost['eur'] = $breur;
+
+$pctcost['cny'] = round((1/$brcny),4);
+
+$pctcost['gold'] = $brgold;
 
 echo json_encode($pctcost);
 
